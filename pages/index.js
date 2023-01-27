0193a6fdab7a91123617c6987b03c81ec43240c5
NotFluyd <leafygyt@gmail.com>
2023-01-27 13:10:38 -0500

    MR BEAST v1

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
         <div className='hero-content text-center'>
           <div className='max-w-lg'>
             <h1 className='text-3xl font-bold'>Firebase Realtime Template Game</h1>
-            <p className='py-6'>This is a template game made by Fluyd for easy development of multiplayer games using Firebase's RealtimeDB.</p>
+            <p className='py-6'>This is a template game made by Fluyd for easy development of multiplayer games using Firebase&apos;s RealtimeDB.</p>
             <div className='grid grid-cols-3 gap-x-2 max-w place-items-center'>
               <button className='btn btn-primary' onClick={() => router.push('/join/enter')}>Join Game</button>
               <div className='divider divider-horizontal' />
