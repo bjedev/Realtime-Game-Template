f2d88484815ebaff660ee6e1836688c70219fe7a
WiiDude <questions.plutonium@gmail.com>
2023-05-04 17:56:58 -0400

    who knows anymore

--- libs/highLevelGameLib.js
+++ libs/highLevelGameLib.js
@@ -40,7 +40,7 @@ export const hostGame = () => {
     // If game Id exists, generate a new one
     if (exists) {
       return false;
-      console.log(`gameId already exists`)
+      console.log("gameId already exists")
     } else {
       createGame(gameId);
       updateState(gameId,"waiting")
--- next.config.js
+++ next.config.js
@@ -2,10 +2,12 @@
 const nextConfig = {
   webpack: (config) => {
     config.resolve.fallback = { fs: false };
-
     return config;
   },
   reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
 }
 
 module.exports = nextConfig
